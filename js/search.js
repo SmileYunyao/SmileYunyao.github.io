de9f99851824b4ff387775125bfcce29e2b873cb
SmileYunyao <3247586491@qq.com>
2022-07-27 23:02:20 +0800

    Site updated: 2022-07-27 23:02:18

--- js/search.js
+++ js/search.js
@@ -1,85 +1,85 @@
-var searchFunc = function (path, search_id, content_id) {
-    'use strict';
-    $.ajax({
-        url: path,
-        dataType: "xml",
-        success: function (xmlResponse) {
-            // get the contents from search data
-            var datas = $("entry", xmlResponse).map(function () {
-                return {
-                    title: $("title", this).text(),
-                    content: $("content", this).text(),
-                    url: $("url", this).text()
-                };
-            }).get();
-            var $input = document.getElementById(search_id);
-            var $resultContent = document.getElementById(content_id);
-            $input.addEventListener('input', function () {
-                var str = '<ul class=\"search-result-list\">';
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                $resultContent.innerHTML = "";
-                if (this.value.trim().length <= 0) {
-                    return;
-                }
-                // perform local searching
-                datas.forEach(function (data) {
-                    var isMatch = true;
-                    var content_index = [];
-                    var data_title = data.title.trim().toLowerCase();
-                    var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
-                    var data_url = data.url;
-                    var index_title = -1;
-                    var index_content = -1;
-                    var first_occur = -1;
-                    // only match artiles with not empty titles and contents
-                    if (data_title != '' && data_content != '') {
-                        keywords.forEach(function (keyword, i) {
-                            index_title = data_title.indexOf(keyword);
-                            index_content = data_content.indexOf(keyword);
-                            if (index_title < 0 && index_content < 0) {
-                                isMatch = false;
-                            } else {
-                                if (index_content < 0) {
-                                    index_content = 0;
-                                }
-                                if (i == 0) {
-                                    first_occur = index_content;
-                                }
-                            }
-                        });
-                    }
-                    // show search results
-                    if (isMatch) {
-                        str += "<li><a href='" + data_url + "' class='search-result-title'>" + data_title + "</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g, "");
-                        if (first_occur >= 0) {
-                            // cut out 100 characters
-                            var start = first_occur - 20;
-                            var end = first_occur + 80;
-                            if (start < 0) {
-                                start = 0;
-                            }
-                            if (start == 0) {
-                                end = 100;
-                            }
-                            if (end > content.length) {
-                                end = content.length;
-                            }
-                            var match_content = content.substr(start, end);
-                            // highlight all keywords
-                            keywords.forEach(function (keyword) {
-                                var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
-                            });
+import $ from 'jquery'
 
-                            str += "<p class=\"search-result\">" + match_content + "...</p>"
+(function () {
+    'use strict';
+
+    const clientWidth = $('body').width()
+    const searchInput = $('#cube-search-input')
+    const searchForm = $('div.cube-search-form')
+    const closeButton = searchForm.find('.cube-close')
+    const searchResult = searchForm.find('.cube-search-result')
+    const formInput = searchForm.find('input.search-input')
+
+    if (clientWidth > 768) {
+
+        if (searchInput) {
+
+            $.getJSON(searchModule.JSONUrl, (json) => {
+                let result = [];
+                formInput.on('input', () => {
+                    let val = formInput.val()
+                    if (val) {
+                        result = getArticles(val, 'posts', json).concat(...getArticles(val, 'pages', json))
+                        displaySearchResult(result)
+                    } else {
+                        // TODO 搜索者可能感兴趣的文章
+                        if (searchResult.find('.search-result-list')) {
+                            searchResult.find('.search-result-list').remove()
                         }
-                        str += "</li>";
                     }
-                });
-                str += "</ul>";
-                $resultContent.innerHTML = str;
-            });
+                })
+            })
+
+            searchInput.on('focus', function () {
+                this.blur()
+                searchForm.addClass('show')
+                formInput.focus()
+            })
+
+            closeButton.on('click', function () {
+                searchForm.removeClass('show')
+            })
         }
-    });
-}
+    }
+
+    function getArticles(val, type, json) {
+        return json[type].filter((e) => {
+            return e.title.indexOf(val) !== -1 || e.text.indexOf(val) !== -1
+        })
+    }
+
+    function displaySearchResult(result) {
+
+        if (searchResult.find('.search-result-list')) {
+            searchResult.find('.search-result-list').remove()
+        }
+
+        const resultList = searchResult.append('<ul class=search-result-list></ul>').find('ul.search-result-list')
+        result.forEach((article) => {
+            if (article.preview) {
+                let isUrl = /https?:\/\//.test(article.preview) ? article.preview: `${searchModule.rootUrl}images/previews/${article.preview}`
+                resultList.append(`<li>
+                                    <a class="search-result-link" 
+                                        href="${searchModule.rootUrl}${article.path}">
+                                        <div class="image" style="background-image: url(${isUrl});"></div>
+                                        <div class="info" style="width: 80%; height: 60px;">
+                                            <p class="title">${article.title}</p>
+                                            <p class="text" style="position: absolute; left: 0; bottom: 0;">${article.text}</p>
+                                        </div>    
+                                    </a>
+                                </li>`)
+            } else {
+                resultList.append(`<li>
+                                    <a class="search-result-link" 
+                                        href="${searchModule.rootUrl}${article.path}">
+                                        <div class="info">
+                                            <p class="title">${article.title}</p>
+                                            <p class="text" style="margin-top: 5px;">${article.text}</p>
+                                        </div>
+                                    </a>
+                                </li>`)
+            }
+        })
+    }
+
+})()
